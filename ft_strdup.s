ef1d249f584077c45796d9f8627cdbd8e5232cde
mli <mli@student.42.fr>
2020-04-06 15:19:56 +0200

    Better ft_strdup

--- ft_strdup.s
+++ ft_strdup.s
@@ -6,7 +6,7 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/27 18:44:49 by mli               #+#    #+#              #
-#    Updated: 2020/03/27 18:44:52 by mli              ###   ########.fr        #
+#    Updated: 2020/04/06 15:18:01 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,11 +17,19 @@ extern _malloc
 		global _ft_strdup
 
 _ft_strdup:
-	push	rdi
+	push	rdi			; save src
+
 	call	_ft_strlen
 	mov		rdi, rax
-	call	_malloc
+	inc		rdi			; len = ft_strlen(s1) + 1
+	call	_malloc		; rax = malloc(ft_strlen(src) + 1)
+
+	cmp		rax, 0		; if (rax == NULL) -> return (NULL)
+	jz		_ft_strdup_end
+
 	mov		rdi, rax
 	pop		rsi
-	call	_ft_strcpy
+	call	_ft_strcpy	; ft_strcpy(dest, src)
+
+_ft_strdup_end:
 	ret
