a0f977cd9bedd40c06a89ecd482005f9f3ac47bf
mli <mli@student.42.fr>
2020-06-02 15:50:05 +0200

    ft_read => errno

--- ft_read.s
+++ ft_read.s
@@ -6,22 +6,22 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/27 16:01:12 by mli               #+#    #+#              #
-#    Updated: 2020/03/27 16:01:44 by mli              ###   ########.fr        #
+#    Updated: 2020/06/02 14:27:03 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 		global _ft_read
+		extern ___error
 		section .text
 
 _ft_read:
-		push	rdx
 		mov		rax, 0x02000003
 		syscall
-		pop		rdx
-		cmp		rax, rdx
-		jne		_ft_read_error
+		jc		exit_error
 		ret
-
-_ft_read_error:
+exit_error:
+		push	rax
+		call	___error
+		pop		QWORD [rax]
 		mov		rax, -1
 		ret
--- test.c
+++ test.c
@@ -7,7 +7,8 @@
 
 #define MAKEITABORT 0
 // Globals
-int g_all = 2, g_done = 0, g_wr = 0;
+int g_all = 2, g_done = 0, g_wr = 0, g_rd = 0;
+int fd;
 
 void	__attribute__((destructor)) ft_destructor();
 
@@ -17,8 +18,10 @@ void	ft_destructor(void)
 	printf("\033[0;94mTests finished!\n");
 	printf("\033[0;96mTotal:\033[0;91m%d\033[0;1m/\033[92m%d\n", g_done, g_all);
 	printf("\033[0;96mft_write: \033[92m%d\n", g_wr);
+	printf("\033[0;96mft_read: \033[92m%d\n", g_rd);
 
 	printf("\033[0m");
+	close(fd);
 }
 
 void	ft_signal(int sign)
@@ -35,12 +38,13 @@ void	truefalse(const int eq[2])
 			eq[0] ? "92mTrue" : "91mFalse", eq[1] ? "92mTrue" : "91mFalse");
 }
 
-int		test_write(const int fd,const char * const str, const int size)
+int		test_write(const int fd, const char * const str, const int size)
 {
 	int err;
 	int ret[2];
 	int eq[2];
 
+	errno = 0;
 	ret[0] = ft_write(fd, str, size);
 	err = errno;
 	ret[1] = write(fd, str, size);
@@ -50,9 +54,58 @@ int		test_write(const int fd,const char * const str, const int size)
 	return (eq[0] && eq[1] ? 1 : 0);
 }
 
+int		test_read(const int fd, const int size)
+{
+	char buf[2][1024];
+	int err;
+	int ret[2];
+	int eq[2];
+
+	errno = 0;
+	bzero(buf[0], sizeof(buf[0]));
+	bzero(buf[1], sizeof(buf[1]));
+	ret[0] = ft_read(fd, buf[0], size);
+	err = errno;
+	ret[1] = read(fd, buf[1], size);
+	eq[0] = (ret[0] == ret[1]); eq[1] = (err == errno);
+	truefalse(eq);
+	g_rd++;
+	return (eq[0] && eq[1] && !strcmp(buf[0], buf[1]) ? 1 : 0);
+}
+
+void	wr_tests(void)
+{
+	assert(test_write(9, "Hello", 5));
+	assert(test_write(fd, "Hello\n", 6));
+	assert(test_write(fd, NULL, 5));
+	assert(test_write(-1, NULL, 5));
+	assert(test_write(fd, "ABCEDFGEFGH", 9));
+	assert(test_write(9, "ABCEDFGEFGH", 9));
+	assert(test_write(fd, NULL, -1));
+	assert(test_write(fd, "A longer sentence\n", 18));
+}
+
+void	rd_tests(void)
+{
+	assert(test_read(9, 5));
+	assert(test_read(9, 9));
+	assert(test_read(fd, 5));
+	assert(test_read(fd, 1));
+	assert(test_read(fd, 9));
+	assert(test_read(fd, 7));
+	assert(test_read(fd, 18));
+}
+
 int		main(void)
 {
+	if ((fd = open("output", O_WRONLY | O_CREAT | O_TRUNC,
+					S_IRWXU | S_IRGRP | S_IROTH)) < 0)
+	{
+		perror(NULL);
+		exit (1);
+	}
 	if (!MAKEITABORT)
 		signal(SIGABRT, ft_signal);
-	assert(test_write(9, "Hello", 4));
+	wr_tests();
+	rd_tests();
 }
