8f3e1c1e15fac9858a0622894e1f08d2228db75c
mli <mli@student.42.fr>
2020-07-06 00:13:35 +0200

    atoi {+/-} are forbidden

--- ft_atoi_base.s
+++ ft_atoi_base.s
@@ -1,19 +1,23 @@
 	global _ft_atoi_base
 	section .text
 
-	; is_white_space according to `man isspace`
-ft_isspace_rdx:
-	cmp [rdx], byte 9
+	; is_white_space according to `man isspace` (+) {+/-}
+ft_is_forbidden_char_rdx:
+	cmp [rdx], byte 9 ; \t
 	je	retone
-	cmp [rdx], byte 10
+	cmp [rdx], byte 10;	\n
 	je	retone
-	cmp [rdx], byte 11
+	cmp [rdx], byte 11;	\v
 	je	retone
-	cmp [rdx], byte 12
+	cmp [rdx], byte 12;	\f
 	je	retone
-	cmp [rdx], byte 13
+	cmp [rdx], byte 13;	\r
 	je	retone
-	cmp [rdx], byte 32
+	cmp [rdx], byte 32;	sp
+	je	retone
+	cmp [rdx], byte 43 ; +
+	je	retone
+	cmp [rdx], byte 45 ; -
 	je	retone
 	jmp retzero
 
@@ -30,16 +34,16 @@ ft_base_check:
 	cmp	[rsi + 1], byte 0
 	jz	error_exit
 
-	; Check if contains white spaces
+	; Check if contains white spaces (+) {+/-}
 	mov rdx, rsi
-ft_nowhitespace:
-	call ft_isspace_rdx
+ft_no_forbidden_c:
+	call ft_is_forbidden_char_rdx
 	cmp rax, 1
 	je	error_exit
 
 	inc	rdx
 	cmp	[rdx], byte 0
-	jne	ft_nowhitespace
+	jne	ft_no_forbidden_c
 
 	mov rax, 42
 	ret
